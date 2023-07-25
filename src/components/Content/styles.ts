8937a0f336220bea9b4f15fead5e09a22cc2fbf8
Iano Maciel <ianomaciel6385@gmail.com>
2023-07-25 13:21:47 -0400

    refactor: ajustes para deixar fixo o aside e mainheader

--- src/components/Content/styles.ts
+++ src/components/Content/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 export const Container = styled.div`
     grid-area: CT;
@@ -6,4 +6,18 @@ export const Container = styled.div`
     color: ${props => props.theme.colors.primary};
 
     background-color: ${props => props.theme.colors.tertiary};
+
+    padding: 30px;
+
+    height: calc(100vh - 70px);
+    overflow-y: scroll;
+
+    ::-webkit-scrollbar {
+        width: 8px;
+    }
+
+    ::-webkit-scrollbar-thumb {
+        background-color: ${props => props.theme.colors.tertiary};
+        border-radius: 4px; /* Borda arredondada da alça */
+    }
 `;
