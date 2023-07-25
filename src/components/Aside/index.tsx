d38d8407fce15e681d67db93c7b92b8937d3572e
Iano Maciel <ianomaciel6385@gmail.com>
2023-07-24 22:38:58 -0400

    feat: desenvolvimento do menu lateral no compente aside

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,9 +1,39 @@
-import { Container } from './styles';
+import React, { useState } from 'react';
+import { Container, MenuContainer, MenuLink } from './styles';
+import {
+    MdPerson,
+    MdOutlinePersonOutline,
+    MdCodeOff,
+    MdCode,
+    MdSchool,
+    MdOutlineSchool,
+} from 'react-icons/md';
 
 export function Aside() {
+    const [clicked, setClicked] = useState('home');
+
+    const toogleMenu = (value: string) => {
+        setClicked(value);
+    };
+
     return (
         <Container>
-            <h1>Aside</h1>
+            <MenuContainer>
+                <MenuLink href="#" onClick={() => toogleMenu('home')}>
+                    {clicked == 'home' ? <MdPerson /> : <MdOutlinePersonOutline />}
+                    Sobre
+                </MenuLink>
+
+                <MenuLink href="#" onClick={() => toogleMenu('projects')}>
+                    {clicked == 'projects' ? <MdCodeOff /> : <MdCode />}
+                    Projetos
+                </MenuLink>
+
+                <MenuLink href="#" onClick={() => toogleMenu('palestras')}>
+                    {clicked == 'palestras' ? <MdSchool /> : <MdOutlineSchool />}
+                    Palestras
+                </MenuLink>
+            </MenuContainer>
         </Container>
     );
 }
--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -7,4 +7,42 @@ export const Container = styled.div`
 
     background-color: ${props => props.theme.colors.tertiary};
     border-right: 1px solid ${props => props.theme.colors.gray};
+
+    display: flex;
+    justify-content: center;
+`;
+
+
+export const MenuContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-top: 70px;
+
+    width: 80%;
+`;
+
+
+export const MenuLink = styled.a`
+    display: flex;
+    align-items: center;
+
+    color: ${props => props.theme.colors.primary};
+    text-decoration: none;
+
+    margin: 10px 0;
+    padding: 10px;
+
+    font-size: 24px;
+    font-weight: 300;
+
+    transition: opacity .3s;
+
+    svg {
+        margin-right: 20px;
+        font-size: 24px;
+    }
+
+    &:hover {
+        opacity: .7;
+    }
 `;
