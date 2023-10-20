242e22b61f8e817f5e88d37969ba6a0d956cd557
Iano Maciel <ianomaciel6385@gmail.com>
2023-10-20 14:52:41 -0400

    feat: ajustes na tela home

--- src/components/Content/styles.ts
+++ src/components/Content/styles.ts
@@ -7,7 +7,7 @@ export const Container = styled.div`
 
     background-color: ${props => props.theme.colors.tertiary};
 
-    padding: 30px 30px;
+    padding: 70px 200px;
 
     height: calc(100vh);
     overflow-y: scroll;
--- src/views/about/styles.ts
+++ src/views/about/styles.ts
@@ -1,62 +1,78 @@
 import { styled } from 'styled-components';
 
 export const Container = styled.div`
-    p {
-        margin-top: 30px;
-        color: ${props => props.theme.colors.secondary};
-        font-size: 25px;
-        font-weight: 300;
-    }
-    h1 {
-        font-size: 40px;
-        color: ${props => props.theme.colors.primary};
-        margin-top: 30px;
-    }
+`;
 
+export const AboutMe = styled.div`
+margin-bottom: 50px;
+    h1 {
+        font-weight: 800;
+        font-size: 50px;
+        color: ${props => props.theme.colors.primary}
+    }
     span {
+        font-weight: 200;
         font-size: 20px;
         color: ${props => props.theme.colors.secondary}
+    }
+`;
+
+export const AboutContent = styled.div`
+    p {
+        font-weight: 200;
+        font-size: 20px;
+        color: ${props => props.theme.colors.secondary};
+
+        margin-bottom: 30px;
     }
 
     a {
         text-decoration: none;
-        font-weight: 800;
-        color: ${props => props.theme.colors.secondary};
+        color: ${props => props.theme.colors.primary};
+        font-weight: 400;
     }
 `;
 
-
 export const Contact = styled.div`
+    margin-top: 40px;
+    margin-bottom: 50px;
+
+    h1 {
+        font-weight: 800;
+        font-size: 50px;
+        color: ${props => props.theme.colors.primary}
+    }
+
+    span {
+        font-weight: 200;
+        font-size: 20px;
+        color: ${props => props.theme.colors.secondary}
+    }
+`;
+
+export const Networks = styled.div`
     display: flex;
-    flex-direction: row;
     justify-content: space-between;
-
-    width: 100%;
-
-    div {
-        width: 70%;
-    }
-
-    p {
-        margin-bottom: 30px;
-    }
-
 `;
 
 export const ContactLink = styled.div`
     display: flex;
     flex-direction: column;
+    width: 50%;
+
+    //background-color: red; // DELETE HERE
 `;
 
 export const LinkContact = styled.a`
     display: flex;
     align-items: center;
 
+    margin-bottom: 15px;
+
     font-size: 24px;
-    font-weight: 300;
+    font-weight: 200;
     text-decoration: none;
     color: ${props => props.theme.colors.secondary};
-    margin: 5px 0;
 
     transition: opacity .4s;
 
@@ -65,12 +81,24 @@ export const LinkContact = styled.a`
     }
 
     svg {
+        font-size: 24px;
         margin-right: 20px;
     }
 `;
 
-export const ContactImage = styled.img`
-    width: 300px;
-    height: 300px;
+export const ProfilePhoto = styled.div`
+    display: flex;
+    position: relative;
+    width: 50%;
+
+    //background-color: blue;
+
+    img {
+        position: absolute;
+        margin-top: -60px;
+        margin-left: 200px;
+        width: 300px;
+        height: 300px;
+    }
 `;
 
