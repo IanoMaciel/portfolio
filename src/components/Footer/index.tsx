0ff6fa9b84499f074fb5a177c999e8dbef93eeaa
IanoMaciel <ianomaciel6385@gmail.com>
2025-07-29 11:21:31 -0400

    feat: criação do component footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,20 @@
+import { CopyrightIcon } from "@phosphor-icons/react";
 import styled from "styled-components";
 
 export default function Footer() {
     return (
         <Container>
-            <h1>Footer</h1>
+            <CopyrightIcon size={24} />
+            <span>Todos os direitos reservados</span>
         </Container>
     );
 }
 
-const Container = styled.footer``;
+const Container = styled.footer`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    color: ${(props) => props.theme.colors.primary};
+    background-color: ${(props) => props.theme.colors.secondary};
+`;
